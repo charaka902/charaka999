37a24be39f67d2fb6879bb301d2805189960c83a
charaka902 <charakazbest88@gmail.com>
2024-10-21 17:02:38 +0530

    Added home view and updated URLs

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import JobSearchView, JobSearchDetailedView, JobSearchBySkillView, JobSearchByTitleView  # Import the new view
 
 urlpatterns = [
+     path('', home, name='home'),
     path('api/job-search/', JobSearchView.as_view(), name='job-search'),
     path('api/job-search-detailed/', JobSearchDetailedView.as_view(), name='job-search-detailed'),
     path('api/job-search-by-skill/', JobSearchBySkillView.as_view(), name='job_search_by_skill'),
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')  # Ensure you have a home.html template
 
 class JobSearchView(APIView):
     def post(self, request):
